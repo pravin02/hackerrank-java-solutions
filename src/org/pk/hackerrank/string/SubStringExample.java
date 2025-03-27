3afec77a4680ae3d5c7d513b41cd76d2528799ab
Pravin <prvnpatil11@gmail.com>
2025-03-27 10:46:08 +0530

    StaircaseProblemSolution

---
+++ src/org/pk/hackerrank/string/SubStringExample.java
@@ -0,0 +1,15 @@
+package org.pk.hackerrank.string;
+
+import java.util.Scanner;
+
+public class SubStringExample {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+
+        System.out.println(S.substring(start, end));
+    }
+}
