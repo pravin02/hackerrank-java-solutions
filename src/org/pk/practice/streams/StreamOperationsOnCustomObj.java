d5edae91688ca9f7b5f071bfab086af0a614628a
Pravin <prvnpatil11@gmail.com>
2025-05-09 23:01:56 +0530

    employee age less than

--- src/org/pk/practice/streams/StreamOperationsOnCustomObj.java
+++ src/org/pk/practice/streams/StreamOperationsOnCustomObj.java
@@ -1,10 +1,13 @@
 package org.pk.practice.streams;
 
 import org.pk.practice.models.Employee;
+import org.pk.practice.models.EmployeeAgeDesignationSalaryDto;
 import org.pk.practice.models.EmployeeDummyList;
 
+import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class StreamOperationsOnCustomObj {
 
@@ -25,6 +28,9 @@ public class StreamOperationsOnCustomObj {
         System.out.println();
 
         System.out.println("Second Lowest Salary Employee\n " + findTheLowestNthSalariedEmployee(employees, 2));
+        System.out.println();
+
+        System.out.println("Employee Age less than 30\n " + findEmployeeAgeLessThan(employees, 30));
         System.out.println();
     }
 
@@ -54,5 +60,29 @@ public class StreamOperationsOnCustomObj {
                 .skip(nth - 1)
                 .findFirst()
                 .orElse(null);
+    }
+
+    static List<EmployeeAgeDesignationSalaryDto> findEmployeeAgeLessThan(List<Employee> employees, int age) {
+        if (age <= 22 || age > 60) {
+            throw new RuntimeException("Age must be between 22 to 60");
+        }
+        return employees
+                .stream()
+                .filter(getAgeLowerThanPredicate(age))
+                .sorted(Comparator.reverseOrder())
+                .map(StreamOperationsOnCustomObj::employeeToDtoMapper)
+                .toList();
+    }
+
+    static EmployeeAgeDesignationSalaryDto employeeToDtoMapper(Employee e) {
+        return new EmployeeAgeDesignationSalaryDto(e.getId(), e.getFirstName(), e.getLastName(), getAge(e.getDob()), e.getDesignation());
+    }
+
+    static Predicate<Employee> getAgeLowerThanPredicate(int age) {
+        return (Employee e) -> getAge(e.getDob()) <= age;
+    }
+
+    static int getAge(LocalDate dob) {
+        return LocalDate.now().getYear() - dob.getYear();
     }
 }
