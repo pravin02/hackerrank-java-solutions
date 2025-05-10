86c8fc97f34b319a34f01c76830f972854fe2000
Pravin <prvnpatil11@gmail.com>
2025-05-10 16:06:11 +0530

    producer consumer with wait and notifyall

--- src/org/pk/practice/concurrency/ProducerConsumerWithCustomQueueAndWaitNotifyAllExample.java
+++ src/org/pk/practice/concurrency/ProducerConsumerWithCustomQueueAndWaitNotifyAllExample.java
@@ -6,28 +6,24 @@ public class ProducerConsumerWithCustomQueueAndWaitNotifyAllExample {
 
     private final CustomQueueWaitAndNotifyAll<Integer> queue = new CustomQueueWaitAndNotifyAll<>();
 
-    final Runnable producer = () -> {
-        IntStream.rangeClosed(1, 5).forEach(no -> {
-            if (Thread.currentThread().getName().split("-")[1].equals("1")) {
-                no = no * 2;
-            }
-            System.out.println(Thread.currentThread().getName() + " Producing no " + no);
-            queue.put(no);
-        });
-    };
-
-    final Runnable producer2 = () -> {
-        IntStream.rangeClosed(1, 5).forEach(no -> {
+    final Runnable producer = () -> IntStream.rangeClosed(1, 5).forEach(no -> {
+        if (Thread.currentThread().getName().split("-")[1].equals("1")) {
             no = no * 2;
-            System.out.println("producer2 " + Thread.currentThread().getName() + " Producing no " + no);
-            queue.put(no);
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    };
+        }
+        System.out.println(Thread.currentThread().getName() + " Producing no " + no);
+        queue.put(no);
+    });
+
+    final Runnable producer2 = () -> IntStream.rangeClosed(1, 5).forEach(no -> {
+        no = no * 2;
+        System.out.println("producer2 " + Thread.currentThread().getName() + " Producing no " + no);
+        queue.put(no);
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    });
 
     final Runnable consumer = () -> {
         while (true) {
