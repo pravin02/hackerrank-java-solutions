1d3c5e051b68b8a767129535a2361061a5eefa54
Pravin <prvnpatil11@gmail.com>
2025-05-10 16:05:02 +0530

    producer consumer with wait and notifyall

---
+++ src/org/pk/practice/concurrency/CustomQueueWaitAndNotifyAll.java
@@ -0,0 +1,47 @@
+package org.pk.practice.concurrency;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class CustomQueueWaitAndNotifyAll<E> {
+    private Queue<E> queue = new LinkedList<>(); // queue to hold records order by added
+    private int max = 10;
+
+    public CustomQueueWaitAndNotifyAll() {
+    }
+
+    public CustomQueueWaitAndNotifyAll(int size) {
+        this.queue = new LinkedList<>();
+        this.max = size;
+    }
+
+    public synchronized void put(E item) {
+        try {
+            while (queue.size() == max) {
+                wait(); // queue is full it will wait for notification to move ahead
+            }
+            queue.add(item);
+            notifyAll(); // now queue is not empty signalling to other threads where they were waiting
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public synchronized E take() {
+        try {
+            while (queue.isEmpty()) {
+                wait(); // queue is empty it will wait here for notification to move ahead
+            }
+
+            E item = queue.remove();
+            notifyAll(); // now queue is not full some item got consumed, signalling to other threads where they were waiting
+            return item;
+        } catch (InterruptedException ie) {
+            throw new RuntimeException(ie);
+        }
+    }
+
+    public int size() {
+        return max;
+    }
+}
