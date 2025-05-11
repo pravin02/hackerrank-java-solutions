0c77039eab6fea0e5925f2eded9dbea7392da557
Pravin <prvnpatil11@gmail.com>
2025-05-11 13:12:18 +0530

    bug fix

--- src/org/pk/hackerrank/string/StringBuilderNull.java
+++ src/org/pk/hackerrank/string/StringBuilderNull.java
@@ -1,16 +1,16 @@
 package org.pk.hackerrank.string;
 
 public class StringBuilderNull {
-	
-	public static void main(String... args) {
-		StringBuilder sb = new StringBuilder();
-		sb.append(null);
-		System.out.println("sb.append(null) " +sb.toString());
-		
-		String s = "welcome";
-		s = null + s;
-		System.out.println("null + s "+s);
-		s = s + null;
-		System.out.println("s + null "+s);		
-	}
+
+    public static void main(String... args) {
+        StringBuilder sb = new StringBuilder();
+        //sb.append(null); // it will throw null pointer exception
+        System.out.println("sb.append(null) " + sb.toString());
+
+        String s = "welcome";
+        s = null + s;
+        System.out.println("null + s " + s);
+        s = s + null;
+        System.out.println("s + null " + s);
+    }
 }
